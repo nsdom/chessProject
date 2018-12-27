5b2cf5b174f304658ee78cf1211c5028cc3f1f6d
Nicolá Domingues <nsdomingues44@gmail.com>
2018-12-27 15:43:42 +0000

    Position class created

--- src/application/program.java
+++ src/application/program.java
@@ -1,11 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Chess project");
-
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
